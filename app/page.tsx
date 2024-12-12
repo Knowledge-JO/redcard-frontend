0dcf941ff3b0137bd4e2e1491f6f127f75ce92fe
Knowledge-JO <megamindtheincredible@gmail.com>
2024-12-12 12:20:17 +0100

    error handling

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/ui/Home";
 import { getChecks } from "@/lib/cryptoApi";
 
 async function Page() {
-  const checks = await getChecks();
+  const checks = (await getChecks()) || [];
   return (
     <div>
       <Home checks={checks} />
